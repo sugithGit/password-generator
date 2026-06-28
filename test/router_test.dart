a5276227be864661151a85d3389ebfc5f4ee5dd6
Sugith K <sugith@trymph.com>
2026-06-28 13:39:45 +0530

    fix: dart fix

--- test/router_test.dart
+++ test/router_test.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:password_generator/core/routes/app_router.dart';
 
 void main() {
   test('Instantiate AppRouter', () {
     final router = AppRouter();
-    print(router.routes.map((e) => e.name).toList());
+    if (kDebugMode) {
+      print(router.routes.map((e) => e.name).toList());
+    }
   });
 }
--- test_router.dart
+++
@@ -1,5 +0,0 @@
-import 'package:password_generator/core/routes/app_router.dart';
-void main() {
-  final router = AppRouter();
-  print(router.routes);
-}
